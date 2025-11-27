aec22add21ccad1d32024bc31f8b75694c0c3a08
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-11-26 19:59:14 -0500

    feat: shacn components, dark mode, Landing page

---
+++ src/landing/LandingPage.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from "react";
+import { Button } from "@/components/ui/button"
+import { ModeToggle } from '@/shared/ModeToggle';
+
+export function LandingPage() {
+  return (
+    <section className="">
+      <Button>Button</Button>
+      <ModeToggle />
+    </section>
+  );
+};
