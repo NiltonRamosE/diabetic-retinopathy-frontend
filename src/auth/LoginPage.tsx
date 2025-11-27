8a568e235c9dc9b43ab5652ed18ebd7982e9f9df
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-11-27 07:37:14 -0500

    feat: agregar login page block

--- src/auth/LoginPage.tsx
+++ src/auth/LoginPage.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
-import { Navbar } from '@/shared/Navbar';
-import { Footer } from '@/shared/Footer';
+import { GalleryVerticalEnd } from "lucide-react"
 
-export const LoginPage = () => {
+import { LoginForm } from "@/auth/components/login-form"
+
+export default function LoginPage() {
   return (
-    <div className="min-h-screen bg-background text-foreground">
-      <Navbar />
-      Login
-      <Footer />
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="/" className="flex items-center gap-2 font-medium">
+            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+              <GalleryVerticalEnd className="size-4" />
+            </div>
+            Clínica Oftalmológica Gismondi
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      <div className="bg-muted relative hidden lg:block">
+        <img
+          src="/placeholder.webp"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
     </div>
-  );
-};
-
-export default LoginPage;
+  )
+}
