75351048b51e429206b83e6f0243ae4c3335c21f
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-11-26 19:58:54 -0500

    build: install shadcn, tailwind, config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    base: '/',
+    site: "https://diabetic-retinopathy-frontend.vercel.app",
+    integrations: [react()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
+});
